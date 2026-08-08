29cf2c8605355812e0bbc30cdd91d5f6e6b50619
kudesv1 <vladimir250motard@gmail.com>
2026-08-08 16:37:38 +0500

    Day 5: Built-in data types, casting, and random numbers

---
+++ day_5.py
@@ -0,0 +1,112 @@
+x = 5
+print(type(x))
+
+x = "Hello World"	# str	
+x = 20	# int	
+x = 20.5	# float	
+x = 1j	# complex	
+x = ["apple", "banana", "cherry"]	# list	
+x = ("apple", "banana", "cherry")	# tuple	
+x = range(6)	# range	
+x = {"name" : "John", "age" : 36}	# dict	
+x = {"apple", "banana", "cherry"}	# set	
+x = frozenset({"apple", "banana", "cherry"})	# frozenset	
+x = True	# bool	
+x = b"Hello"	# bytes	
+x = bytearray(5)	# bytearray	
+x = memoryview(bytes(5))	# memoryview	
+x = None	# NoneType	
+
+x = str("Hello World")	# str	
+x = int(20)	# int	
+x = float(20.5)	# float	
+x = complex(1j)	# complex	
+x = list(("apple", "banana", "cherry"))	# list	
+x = tuple(("apple", "banana", "cherry"))	# tuple	
+x = range(6)	# range	
+x = dict(name="John", age=36)	# dict	
+x = set(("apple", "banana", "cherry"))	# set	
+x = frozenset(("apple", "banana", "cherry"))	# frozenset	
+x = bool(5)	# bool	
+x = bytes(5)	# bytes	
+x = bytearray(5)	# bytearray	
+x = memoryview(bytes(5))	# memoryview	
+
+x = 5
+y = 3.14
+z = "Hello"
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+# convert from int to float:
+a = float(x)
+
+# convert from float to int:
+b = int(y)
+
+# convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+
+print(random.randrange(1, 10))
+
+x = 5
+y = 3.14
+z = 2+3j
+print(type(x))
+print(type(y))
+print(type(z))
