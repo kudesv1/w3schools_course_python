b48777ca6d0e4c6d9c8689be4faa1ee4d43b0f14
kudesv1 <vladimir250motard@gmail.com>
2026-08-08 16:36:20 +0500

    Day 2: Basic syntax, indentation, and print formatting

---
+++ day_2.py
@@ -0,0 +1,34 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+print("Hello World!")
+
+print("Have a good day.")
+
+print("Learning Python is fun!")
+
+print("Hello World!")
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print("This will work!")
+print('This will also work!')
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
