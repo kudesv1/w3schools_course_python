58eab97770677d1e258771d06939179d3dd35eda
kudesv1 <vladimir250motard@gmail.com>
2026-08-08 16:36:46 +0500

    Day 3: Numbers, basic math operations, and comments

---
+++ day_3.py
@@ -0,0 +1,40 @@
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
+
+print("I am", 25)
+
+# This is a comment
+print("Hello, World!")
+
+print("Hello, World!") # This is a comment
+
+# print("Hello, World!")
+print("Cheers, Mate!")
+
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# This is a comment
+
+# print("This should not run")
+
+"""
+This is
+a multiline
+comment
+"""
